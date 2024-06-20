bd52c1422e244861f3f315135c8eeecacb2f4749
Theo Truong <theotr@amazon.com>
2024-06-20 14:09:46 -0600

    #

--- api_generator/src/playground.ts
+++ api_generator/src/playground.ts
@@ -8,20 +8,7 @@
  */
 
 import Generator from './Generator'
-import FunctionFileRenderer from './renderers/FunctionFileRenderer'
-import ModuleFileRenderer from './renderers/ModuleFileRenderer'
-import IndexFileRenderer from './renderers/IndexFileRenderer'
 
 const generator = new Generator('opensearch-openapi.yaml', './test_site')
 
 generator.generate()
-
-// const ns = generator.namespaces.rollups
-// const func_file = new FunctionFileRenderer(ns.operation_groups.explain, ns)
-// console.log(func_file.render())
-//
-// const mod_file = new ModuleFileRenderer(ns)
-// console.log(mod_file.render())
-//
-// const idx_file = new IndexFileRenderer(generator.namespaces)
-// console.log(idx_file.render())
