d09f144cecbfdba924500cfb6d03fcbacd67ca1d
Theo Truong <theotr@amazon.com>
2024-06-20 16:05:18 -0600

    #

--- api_generator/src/playground.ts
+++ api_generator/src/playground.ts
@@ -12,3 +12,21 @@ import Generator from './Generator'
 const generator = new Generator('opensearch-openapi.yaml', '/Users/theotr/IdeaProjects/opensearch-js-base')
 
 generator.generate()
+
+// Memory usage:
+//   RSS: 23.16 MB
+//   Heap Total: 3.91 MB
+//   Heap Used: 3.01 MB
+//   External: 1.02 MB
+
+// Memory usage:
+//   RSS: 72.43 MB
+//   Heap Total: 34.63 MB
+//   Heap Used: 21.7 MB
+//   External: 1.92 MB
+
+// Memory usage:
+//   RSS: 65.27 MB
+//   Heap Total: 31.41 MB
+//   Heap Used: 16.14 MB
+//   External: 1.92 MB
